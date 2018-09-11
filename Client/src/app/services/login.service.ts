cf7d500a7d2cc4031dee9485308bd5194a593df6
AscDbM <DoberenzM@web.de>
2018-09-11 09:26:02 +0200

    finished login

--- Client/src/app/services/login.service.ts
+++ Client/src/app/services/login.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 
 import { User } from '../Models/user';
 import { Observable } from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable({
   providedIn: 'root'
@@ -13,18 +17,19 @@ export class LoginService {
   constructor(private http: HttpClient) { }
 
   //TODO: change to actual path
-  private apiUrl = "";
+  private apiUrl = "http://localhost:50000/api";
 
-  
+
+  //remember to kill fakeDB
   //TODO: change to actual path  
   login(username: string, password: string) {
-     return this.http.post<any>(`${this.apiUrl}/users/authenticate`, { username: username, password: password })
+     return this.http.post<any>(`${this.apiUrl}/Login`, { username: username, password: password }, httpOptions)
        .pipe(map(user => {
         	if(user && user.token) {
             localStorage.setItem("currentUser", JSON.stringify(user));
           }
           return user;
-      }));   
+      }));      
     
     }
 
