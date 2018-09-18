15e2ca4897d7ac6d091f78d8631759e3c22d7228
NickRegnard <nikiliev23@gmail.com>
2018-09-18 15:03:11 +0200

    maybeneeded

---
+++ Filmothek/Models/ModeratorHistoryMask.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Filmothek.Models
+{
+    public class ModeratorHistoryMask
+    {
+        public int ModeratorId { get; set; }
+        public string Activity { get;set; }
+        public DateTime Date { get; set; }
+
+        public ModeratorHistoryMask(int Id, string Activity, DateTime Date)
+        {
+            this.ModeratorId = ModeratorId;
+            this.Activity = Activity;
+            this.Date = Date;
+        }
+
+        public ModeratorHistoryMask() { }
+    }
+}
